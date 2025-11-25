1188e8bff9d32ffdc2cbc3ebbefe4aacf4d6d9c7
ahmedeldeeb3 <ahmedmohamed3121999@gmail.com>
2025-11-25 19:03:31 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet ,Home],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
