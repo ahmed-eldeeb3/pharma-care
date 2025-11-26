5251d89db7a54c93d769282ecb3d821c8bf1617d
Ahmedkhaled <aboelsoudahmed345@gmail.com>
2025-11-26 03:15:34 +0200

    order history

---
+++ src/app/order-history/component/orderdetails-3/orderdetails-3.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-orderdetails-3',
+  standalone: true,
+  imports: [],
+  templateUrl: './orderdetails-3.component.html',
+  styleUrl: './orderdetails-3.component.css'
+})
+export class Orderdetails3Component {
+
+}
---
+++ src/app/order-history/component/orderhis-component/orderhis-component.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+
+@Component({
+  selector: 'app-orderhis-component',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './orderhis-component.component.html',
+  styleUrl: './orderhis-component.component.css'
+})
+export class OrderhisComponentComponent {
+
+}
